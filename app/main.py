1902fb7f147a7072d8c20ca02fb5f3105a62d20b
omar <omarmohhameed828@gmail.com>
2024-02-17 09:33:23 +0200

    restructure project files

--- app/main.py
+++ app/main.py
@@ -13,9 +13,9 @@ class Post(BaseModel):
     rating: Optional[int] = None
 
 def find_post_index(id):
-    for post in my_posts:
+    for i, post in enumerate(my_posts):
         if post['id'] == id:
-            return id
+            return i
 
 my_posts = [
     {"title": "title of post 1", "content": "content of post 1", "id": 1},
@@ -68,3 +68,15 @@ def delete_post(id: int):
     # If the post is found, delete it (you may want to implement the deletion logic here)
     # For now, the code raises a 204 No Content response without deleting anything
     return Response(status_code=status.HTTP_204_NO_CONTENT)
+
+@app.put('/posts/{id}')
+def update_post(id: int, post: Post):
+    index = find_post_index(id)
+    if index is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='ID Not Exists')
+
+    post_dict = post.model_dump()
+    post_dict['id'] = id
+    my_posts[index] = post_dict
+
+    return {"Message": post_dict}
