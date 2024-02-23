04fa7f33f2d2413761cbb2320495d5e2208dbc9e
omar <omarmohhameed828@gmail.com>
2024-02-23 13:22:43 +0200

    update ignore file

--- app/main.py
+++ app/main.py
@@ -1,13 +1,10 @@
-from fastapi import FastAPI, Response, status, HTTPException, Depends
-from fastapi.params import Body
-from sqlalchemy.orm import Session
-from typing import Optional, List
-from random import randrange
+# uvicorn app.main:app --reload
+from fastapi import FastAPI
 import psycopg2
 from psycopg2.extras import RealDictCursor
 import time
-from . import models, schemas, utils
-from .database import engine, SessionLocal
+from . import models
+from .database import engine
 
 from .routers import post, user, auth
 
