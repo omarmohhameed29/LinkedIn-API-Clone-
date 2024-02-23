535daa0620280a7540386f3e5e1daa30aa97cc23
omar <omarmohhameed828@gmail.com>
2024-02-23 14:20:04 +0200

    handle login before post

--- app/routers/post.py
+++ app/routers/post.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI, Response, status, HTTPException, Depends, APIRouter
 from sqlalchemy.orm import Session
-from .. import models, schemas, utils
+from .. import models, schemas, utils, oauth2
 from ..database import engine, get_db
 from typing import List
 
@@ -24,7 +24,9 @@ def get_post(id: int, db: Session = Depends(get_db)):
 
 
 @router.post('/', status_code=status.HTTP_201_CREATED, response_model=schemas.Post)
-def create_post(post: schemas.PostCreate, db: Session = Depends(get_db)):
+def create_post(post: schemas.PostCreate, db: Session = Depends(get_db), user_id : int = Depends(oauth2.get_current_user)):
+
+    print(user_id)
     db_item = models.Post(**post.model_dump())
     db.add(db_item)
     db.commit()
--- app/schemas.py
+++ app/schemas.py
@@ -39,4 +39,4 @@ class Token(BaseModel):
     token_type: str
 
 class TokenData(BaseModel):
-    id: Optional[str] = None
+    id: Optional[int] = None
