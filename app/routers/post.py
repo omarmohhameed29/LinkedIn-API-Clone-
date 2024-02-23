e557c4c631a6b7344fff9d738d4e09aaf0c2faf7
omar <omarmohhameed828@gmail.com>
2024-02-23 14:30:24 +0200

    testing expiration time of token

--- app/routers/post.py
+++ app/routers/post.py
@@ -11,12 +11,12 @@ router = APIRouter(
 
 
 @router.get('/', response_model=List[schemas.Post])
-def get_posts(db: Session = Depends(get_db)):
+def get_posts(db: Session = Depends(get_db), user_id : int = Depends(oauth2.get_current_user)):
     return db.query(models.Post).all()
 
 
 @router.get('/{id}', response_model=schemas.Post)
-def get_post(id: int, db: Session = Depends(get_db)):
+def get_post(id: int, db: Session = Depends(get_db), user_id : int = Depends(oauth2.get_current_user)):
     post = db.query(models.Post).filter(models.Post.id == id).first()
     if post:
         return post
@@ -38,7 +38,7 @@ def create_post(post: schemas.PostCreate, db: Session = Depends(get_db), user_id
 
 
 @router.delete('/{id}', status_code=status.HTTP_204_NO_CONTENT)
-def delete_post(id: int, db: Session = Depends(get_db)):
+def delete_post(id: int, db: Session = Depends(get_db), user_id : int = Depends(oauth2.get_current_user)):
     deleted_post = db.query(models.Post).filter(models.Post.id == id)
     if deleted_post.first() is None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="ID Not Exists")
@@ -47,7 +47,7 @@ def delete_post(id: int, db: Session = Depends(get_db)):
     db.commit()
     
 @router.put('/{id}', response_model=schemas.Post)
-def update_post(id: int, updated_post: schemas.PostCreate, db: Session = Depends(get_db)):
+def update_post(id: int, updated_post: schemas.PostCreate, db: Session = Depends(get_db), user_id : int = Depends(oauth2.get_current_user)):
     post_query = db.query(models.Post).filter(models.Post.id == id)
     post = post_query.first()
 
