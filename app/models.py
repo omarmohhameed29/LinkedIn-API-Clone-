80a18e71db99c3dc3745b6623afb88fae3cd7b6a
omar <omarmohhameed828@gmail.com>
2024-02-27 10:26:52 +0200

    set middleware access list

--- app/models.py
+++ app/models.py
@@ -25,6 +25,7 @@ class User(Base):
     email = Column(String, nullable=False, unique=True)
     password = Column(String, nullable=False)
     created_at = Column(TIMESTAMP(timezone=True), server_default=func.now(), nullable=False)
+    phone_number = Column(String)
 
 class Vote(Base):
     __tablename__ = "votes"
