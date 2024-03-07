9c51b49afe44c4767eaca2e3564afb4ba860f845
omarmohhameed29 <omarmohhameed828@gmail.com>
2024-03-07 06:04:15 +0200

    init test for database and user

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -11,7 +11,7 @@ def login(user_credentials:OAuth2PasswordRequestForm = Depends(), db: Session = 
     user = db.query(models.User).filter(models.User.email == user_credentials.username).first()
 
     if not user:
-        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail='Invalid Credentials')
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail='Invalid Credentials!')
     
     if not utils.verify(user_credentials.password, user.password):
         raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail='Invalid Credentials')
--- tests/test_users.py
+++ tests/test_users.py
@@ -1,45 +1,18 @@
-from fastapi.testclient import TestClient
-from app.main import app
 from app import schemas
-from app.config import settings
-from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
-from app.database import get_db, Base
+from .database import client, session
 
-SQLALCHEMY_DATABASE_URL = f"postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}_test"
-
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
-
-TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-
-Base.metadata.create_all(bind=engine)
-
-# Dependency
-def override_get_db():
-    db = TestingSessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
-
-
-
-app.dependency_overrides[get_db] = override_get_db
-
-
-client = TestClient(app)
-
-
-def test_root():
+def test_root(client):
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {'message': 'Welcome to LinkedIn API'}
 
 
-def test_create_user():
+def test_create_user(client):
     response = client.post('/users/', json={"email":"omarmo@gmail.com", "password": "omarmo"})
     new_user = schemas.UserOut(**response.json())
     assert new_user.email == "omarmo@gmail.com"
     assert response.status_code == 201
+
+def test_user_login(client, session):
+    response = client.post('/login', data={"username": "omarmo@gmail.com", "password": "omarmo"})
+    assert response.status_code == 200
